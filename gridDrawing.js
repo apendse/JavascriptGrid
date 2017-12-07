9289956b77850b42689488283eee49140055d852
Ashutosh <apendse@gmail.com>
2017-12-07 11:59:04 -0800

    Alternate dashes for diagonal

--- gridDrawing.js
+++ gridDrawing.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById("myCanvas");
 
-canvas.width = window.innerWidth - 10;
-canvas.height = window.innerHeight - 10;
+canvas.width = window.innerWidth - 5;
+canvas.height = window.innerHeight - 5;
 var context = canvas.getContext("2d");
 
 console.log("I am here  " + canvas.width + " " + canvas.height);
@@ -14,17 +14,18 @@ var gridWidth = 40;
 var gridHeight = 40;
 var iterations = 100;
 //vertical and horizontal
+var dash = [5, 5];
 for (var i = 0; i < iterations; i++) {
 
-    if (i % 2 == 0) {
+    if (i % 2 == 1) {
         context.beginPath();
-        context.setLineDash([5, 5]);
+        context.setLineDash(dash);
         context.moveTo(0, i * gridHeight);
         context.lineTo(width, i * gridHeight);
         context.stroke();
 
         context.beginPath();
-        context.setLineDash([5, 5]);
+        context.setLineDash(dash);
         context.moveTo(i*gridWidth, 0);
         context.lineTo(i*gridWidth, height);
         context.stroke();
@@ -45,43 +46,56 @@ for (var i = 0; i < iterations; i++) {
 }
 
 // diagonal lines
+// downward diagonals
 for (var i = 0; i < iterations; i++) {
+    // line starting at x axis
     var x = i*gridWidth;
     var y = 0;
     var xEnd = i*gridWidth + iterations*gridWidth;
     var yEnd = iterations*gridHeight;
-    
-    
+    var dash = i % 2 == 0 ? [] : [5, 5];
+    context.beginPath();
+    context.setLineDash(dash);
     context.moveTo(x, y);
     context.lineTo(xEnd, yEnd);
+    context.stroke();
 
+    // Line starting at y axis
     x = 0;
     y = (i + 1)*gridHeight;
     xEnd = x + iterations*gridWidth;
     yEnd = y + iterations*gridHeight;
 
+    context.beginPath();
+    context.setLineDash(dash);
     context.moveTo(x, y);
-    
     context.lineTo(xEnd, yEnd);
+    context.stroke();
+    
 }
 
+// upward diagonals
 for (var i = 0; i < iterations; i++) {
     var x = i*gridWidth;
     var y = 0;
     var xEnd = x - iterations*gridWidth;
     var yEnd = y + iterations*gridHeight;
+    var dash = i % 2 == 0 ? [] : [5, 5];
     
-    
+    context.beginPath();
+    context.setLineDash(dash);
     context.moveTo(x, y);
-
     context.lineTo(xEnd, yEnd);
+    context.stroke();
     x = 0;
     y = (i + 1)*gridHeight;
     xEnd = x - iterations*gridWidth;
     yEnd = y + iterations*gridHeight;
-
+    context.beginPath();
+    context.setLineDash(dash);
     context.moveTo(x, y);
     context.lineTo(xEnd, yEnd);
+    context.stroke();
     
 }
 
